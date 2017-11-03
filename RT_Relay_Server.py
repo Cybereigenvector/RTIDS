f9a4f2ce6bfda017e0ad1995db3f850f889da084
Rishabh Das <das.rishabh92@gmail.com>
2017-11-02 23:29:09 -0500

    Machine learning working! The handling after detection of attack needs to be fixed

--- RT_Relay_Server.py
+++ RT_Relay_Server.py
@@ -15,6 +15,7 @@
 #2.Network anomaly
 #
 #@Rishabh Das
+#@Proxy Server code based on Ricardo Pascal's relay server code
 #Date:-2nd November 2017 
 #------------------------------------------------------------------------------------------
 import socket
@@ -122,34 +123,39 @@ class TheServer:
                 x=(self.current-self.prev).microseconds
                 self.prev=self.current
 
-                if(len(self.dataset)<20 and self.trained==0 ):
+                if(len(self.dataset)<20 and self.trained==0):
                     if (self.s.getpeername()[0] not in self.white_list):
                             self.white_list.append(self.s.getpeername()[0])
                     if(self.s.getpeername()[0]=='127.0.0.1' and self.trained == 0 ):
                         self.dataset.append(x)
                         print((x),self.s.getpeername()[0])
                         print("Whitelist->",self.white_list)
-                elif (self.lock==0 or self.final==1):
+                elif (self.lock==0):
                     print("Data is ready for training")
                     Algorithm_trainer = threading.Thread(target=self.train)
                     self.lock=1
+                    self.trained=0
                     Algorithm_trainer.start()
                 if(self.trained==1):
-                    print (self.cluster_center-200,x)
-                    if(len(self.dataset)<200 and self.s.getpeername()[0]=='127.0.0.1'):
-                        self.dataset.append(x)
-                    elif self.final==0:
-                        self.final=1
-                    if (self.s.getpeername()[0] not in self.white_list):
+                    #print("Hello from classifier")
+                    #print (self.cluster_center-200,x)
+                    if(len(self.dataset)<20 and self.final!=2):
+                        if(self.s.getpeername()[0]=='127.0.0.1'):
+                            self.dataset.append(x)
+                            print(len(self.dataset))
+                    elif(self.final==1 and self.lock==1):
+                        self.lock=0
+                    if (self.s.getpeername()[0] not in self.white_list and self.s.getpeername()[0] not in self.new_connections):
                             self.new_connections.append(self.s.getpeername()[0])
-                    if(self.s.getpeername()[0]=='127.0.0.1' and x<(self.cluster_center-200)):
+                    if(self.s.getpeername()[0]=='127.0.0.1' and x<(self.cluster_center-200) and self.final==2):
                         self.count=self.count+1
                         print(x)
                         if(self.count==10):
                             print("Possible DOS attack!!\nNOTE:-The server was stopped to protect the PLC from DOS attack\nService would be restarted after 5 sec")
                             print("Supected IPs ",self.new_connections)
+                            self.flag=1
 
-                        '''     if(self.alive==0):
+                        '''if(self.alive==0):
                              count_reset_handler = threading.Thread(target=self.count_reset)
                              self.alive=1
 '''
@@ -212,14 +218,14 @@ class TheServer:
         #Verify the performance using coefficient
         self.cluster_center=self.kmeans.cluster_centers_
         print(self.cluster_center)
-        self.trained=1
         del self.dataset[:]
-        if(self.final==1):
-            self.final=2
+        self.final=self.final+1
+        if(self.final==2):
             ##setting final training flag after this step the agorithm is fully functional
             print("RT_IDS Started Monitoring!!")
         else:
             print("RT_IDS Started callibrating!!")
+        self.trained=1
 
 #------------------------------------------------------------------------------------------
 #The dataset is prepared realtime in this framework and once ready the dataset is used for 
