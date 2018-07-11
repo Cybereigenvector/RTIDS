c757e126283641db21f5ecd0ebad40e376c65ba7
Rishabh Das <das.rishabh92@gmail.com>
2018-07-11 17:02:44 +0000

    Ethernet module corrected

--- NetworkModules/ethernet.py
+++ NetworkModules/ethernet.py
@@ -1,14 +1,24 @@
+# -----------------------------------------------------------------------------------
+# This module of the IDS is designed to unpack the Ethernet header of the incoming
+# network packets
+# Created by Rishabh Das
+# Date 11th July 2018
+# -----------------------------------------------------------------------------------
 import socket
 import struct
 from general import *
 
-
+# This class is invoked from the main sniffer program to unpack the ethernet header portion of the network packet
 class Ethernet:
 
     def __init__(self, raw_data):
-
+        # 6s Siginifies that the Mac address is 6 characters long and the H is
+        # for the single unsigned integer for the protocol. Only first 14 bytes
+        # are considered for unpacking
         dest, src, prototype = struct.unpack('! 6s 6s H', raw_data[:14])
 
+        # Storing the unpacked addresses and the packet header details in the variables
+        # This will be accessed from the main sniffer program
         self.dest_mac = get_mac_addr(dest)
         self.src_mac = get_mac_addr(src)
         self.proto = socket.htons(prototype)
