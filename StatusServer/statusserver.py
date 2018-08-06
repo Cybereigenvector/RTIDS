09bcd6169f2f1d800fb9dce9d65e12d2b11955a0
Rishabh Das <das.rishabh92@gmail.com>
2018-08-06 21:55:13 +0000

    Creating server

--- StatusServer/statusserver.py
+++ StatusServer/statusserver.py
@@ -15,3 +15,54 @@
 # ->
 # By:- Rishabh Das
 # ----------------------------------------------------------------
+
+import socket
+import threading
+
+class IPSserver:
+
+    # ------------------------------------------------------------
+    # Initializes the variables that would be used through out the
+    # program. This is the constructor
+    # ------------------------------------------------------------
+
+    def __init__(self,prt=5001,addr='127.0.0.1'):
+        self.port=prt
+        self.ip=addr
+
+    # ------------------------------------------------------------
+    # This function creates the server. The server starts listening
+    # on the defined  port and the IP once the create server
+    # function is invoked by the calling program
+    # ------------------------------------------------------------
+
+    def create_server(self):
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            print("Socket created successfully!")
+
+        except socket.error as err:
+            print("Socket creation error!!")
+        
+        try:
+            sock.bind((self.ip, self.port))
+            sock.listen(1)
+
+        except socket.error as err:
+
+
+
+    def start_server(self):
+        try:
+            s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            print("Socket successfully created!")
+        except socket.error as err:
+            print("Socket creation error!!")
+
+
+    def stopserver(self):
+
+
+
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
