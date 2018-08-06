bbea73f0edc214783ceb0e7fa3978b0dd71b1853
Rishabh Das <das.rishabh92@gmail.com>
2018-08-06 19:58:49 +0000

    Purpose of Status server added

---
+++ StatusServer/statusserver.py
@@ -0,0 +1,17 @@
+# ----------------------------------------------------------------
+# This is the server module of the IPS that replies to any client
+# that tries to connect to the server. Specific commands are
+# supported by the server. These commands replies the clients with
+# specific information about the IPS
+# -> Receives the messages from the clients
+# -> Replies with the status of the IPS
+#
+# Future Plans:-
+# -> Find the configuration of the IPS
+# -> A number of custom messages and commands will be interpreted
+#    with the IPS
+#
+# Dependencies:-
+# ->
+# By:- Rishabh Das
+# ----------------------------------------------------------------
