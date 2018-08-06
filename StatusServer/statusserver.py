167bde6d911f173582c43512e669076f8c2bc352
Rishabh Das <das.rishabh92@gmail.com>
2018-08-06 23:35:53 +0000

    Echo server working.

--- StatusServer/statusserver.py
+++ StatusServer/statusserver.py
@@ -17,6 +17,7 @@
 # ----------------------------------------------------------------
 
 import socket
+import sys
 import threading
 
 class IPSserver:
@@ -29,40 +30,68 @@ class IPSserver:
     def __init__(self,prt=5001,addr='127.0.0.1'):
         self.port=prt
         self.ip=addr
-
+        self.connections = []
     # ------------------------------------------------------------
     # This function creates the server. The server starts listening
     # on the defined  port and the IP once the create server
     # function is invoked by the calling program
     # ------------------------------------------------------------
 
-    def create_server(self):
+    def initialize_server(self):
         try:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             print("Socket created successfully!")
 
         except socket.error as err:
             print("Socket creation error!!")
-        
+            sys.exit()
+
         try:
-            sock.bind((self.ip, self.port))
-            sock.listen(1)
+            self.sock.bind((self.ip, self.port))
+            print("Socket binded successfully!")
 
         except socket.error as err:
+            print("Socket failed to bind!!")
+            sys.exit()
 
+        self.sock.listen(10)
+        print("Server initialization successful!")
 
+    # -----------------------------------------------------------
+    # This handler function is executed by the thread assigned to
+    # client. This function receives data and the calls the
+    # necessary functions. This function also sends appropriate
+    # reply to the
+    # -----------------------------------------------------------
+
+    def client_handler(self,client,client_addr):
+        while True:
+            data = client.recv(1024)
+            for connection in self.connections:
+                connection.send(data)
+            if not data:
+                break
+            print(threading.currentThread().getName() +": "+ data)
+
+    # -----------------------------------------------------------
+    # This function receives the clients and assigns a handler to
+    # the clients.
+    # -----------------------------------------------------------
 
     def start_server(self):
-        try:
-            s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            print("Socket successfully created!")
-        except socket.error as err:
-            print("Socket creation error!!")
+        i=0
+        print("Listening on " + str(self.ip) + " port " + str(self.port))
+        while True:
+            client, client_addr = self.sock.accept()
+            i=i+1
+            clientthread = threading.Thread(target=self.client_handler,args=(client, client_addr),name="Client " + str(i))
+            clientthread.daemon = True
+            clientthread.start()
+            self.connections.append(client)
+            print(self.connections)
 
 
-    def stopserver(self):
-
-
-
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ins= IPSserver(5002,"127.0.0.1")
+ins.initialize_server()
+ins.start_server()
+#ins.client_handler()
